8777ff6b123945ef1fd4768aee9e95aebb6958f1
Ihor Kobzar <supermensy@gmail.com>
2024-12-04 20:21:25 +0200

    Add task 26

---
+++ src/26. Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] y = [1, 1, 2, 2, 2, 2, 3, 4, 5, 5, 5, 6, 6, 7];
+Console.WriteLine(new Solution().RemoveDuplicates(y));
+Console.WriteLine("- - - - - -");
+foreach (var i in y)
+{
+    Console.WriteLine(i);
+}
+
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        var currentNum = -101;
+        var putIdx = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > currentNum)
+            {
+                currentNum = nums[i];
+                nums[putIdx++] = currentNum;
+            }
+        }
+
+        return putIdx;
+    }
+}
