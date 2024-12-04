a192cd5a8e238d7241ea56a1ecda43d4515aba77
Ihor Kobzar <supermensy@gmail.com>
2024-12-04 21:01:55 +0200

    Add task 28

---
+++ src/28. Find the Index of the First Occurrence in a String/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine(new Solution().StrStr("sadbutsad", "sad"));
+Console.WriteLine(new Solution().StrStr("aaa", "aaaa"));
+
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        for (var i = 0; i < haystack.Length; i++)
+        {
+            if (haystack[i] == needle[0])
+            {
+                var ths = true;
+
+                for (var j = 1; j < needle.Length; j++)
+                {
+                    if (i + j > haystack.Length - 1 || haystack[i + j] != needle[j])
+                    {
+                        ths = false;
+                        break;
+                    }
+                }
+
+                if (ths)
+                {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
